6d6b5e81317abb9a925eb480b0e4abd14e14390e
dumanskyi <dumanskyi.arthur@gmail.com>
2019-11-20 14:29:36 +0200

    task_18

--- task_18/js/main.js
+++ task_18/js/main.js
@@ -3,17 +3,20 @@
     const buttonAdd = document.getElementById('addTask');
     const addSound = document.getElementById("addsound");
     const $ul = document.querySelector('.notesList');
- 
-    let tasks = [];
+    const isData = localStorage.getItem('notes');
 
-    if (localStorage.getItem('notes')){
-        tasks = JSON.parse(localStorage.getItem('notes'))
+    const todo = {
+        tasks : (isData ? (JSON.parse(isData)).tasks : []),
+        isShowDone : (isData ? (JSON.parse(isData)).isShowDone : true)
     };
+    if (!todo.isShowDone) {
+        $ul.classList.add('hidden')
+    }
 
-    // NOTE CREATION (DOM)
-    // -----------------
-    // -----------------
-    
+    // NOTES CREATION
+    // ---------------------
+    // ---------------------
+
     const creation = function({taskDiscription, taskIsDone}) {
         
         $task = document.createElement('li');
@@ -26,10 +29,9 @@
         $textBlock.classList.add(`textBlock`);
         $task.append($textBlock);
         $textBlock.innerText = `${taskDiscription}`;
-    
+
         let $buttonsBlock = document.createElement('div');
         $buttonsBlock.classList.add('buttonsBlock');
-
         let $statusButton = document.createElement('button');
         $statusButton.classList.add(`pendingButton`);
         $statusButton.innerHTML = '&#10008;';
@@ -37,36 +39,31 @@
             $statusButton.classList.add('doneButton');
             $statusButton.innerHTML = '&#10004;'; 
         }
-
         let $editButton = document.createElement('button');
         $editButton.classList.add('editButton');
         $editButton.innerHTML = '&#9998;';
-
         let $deleteButton = document.createElement('button');
         $deleteButton.classList.add('delete');
         $deleteButton.innerHTML = '&#128465;';
-    
+
         $task.append($buttonsBlock);
         $buttonsBlock.append($statusButton);
         $buttonsBlock.append($editButton);
-        $buttonsBlock.append($deleteButton);  
-            
+        $buttonsBlock.append($deleteButton);          
+    };
+    todo.tasks.forEach(note => (creation(note)));
+    let saveToLS = function(key, obj){
+        localStorage.setItem(key, JSON.stringify(obj));
     }
-
-    tasks.forEach(note => (creation(note)));
-
-    const saveToLS = (data, key='notes') => localStorage.setItem(key, JSON.stringify(data));
 
     // EVENTS
     // ---------------------
     // ---------------------
-
     const form = document.querySelector('.noteForm');
     form.addEventListener('submit', e => {
-
         e.preventDefault();
         let someTaskValue = someTask.value.trim();
-        let index = tasks.findIndex((el) => el.taskDiscription === someTaskValue);
+        let index = todo.tasks.findIndex((el) => el.taskDiscription === someTaskValue);
         if (index === -1){
             if (someTaskValue !== ''){
                 addSound.play();
@@ -76,14 +73,14 @@
                 });
                 someTask.value = '';
                 const currentDate = new Date;
-                tasks.push({
+                todo.tasks.push({
                     taskId: moment(currentDate).format("x"),
                     taskDiscription: someTaskValue,
                     taskIsDone: false,
                     dateOfCreation: moment(currentDate).format("DD MMM YYYY"),
                     timeOfCreation: moment(currentDate).format("hh:mm")
                 });
-                saveToLS(tasks);
+                saveToLS('notes', todo);
             } else {
                 alert('Нельзя вводить пустую заметку');
             }    
@@ -92,75 +89,71 @@
             someTask.value = '';
         }   
     });
-
     $ul.addEventListener('click', event => {
         $task = event.target.closest('.item');
         let taskBlock = event.target.closest('.buttonsBlock').previousSibling;
         let taskText = taskBlock.innerText;
-
         if (event.target.className === 'pendingButton'){
             addSound.play()
-            index = tasks.findIndex((el) => el.taskDiscription === taskText);
-            tasks[index].taskIsDone = true;
+            index = todo.tasks.findIndex((el) => el.taskDiscription === taskText);
+            todo.tasks[index].taskIsDone = true;
             $task.classList.toggle('done');
             event.target.classList.toggle('doneButton');
             event.target.innerHTML = '&#10004;';
-            saveToLS(tasks);
-
+            saveToLS('notes', todo);
         } else if (event.target.className === 'pendingButton doneButton'){
             addSound.play()
-            index = tasks.findIndex((el) => el.taskDiscription === taskText);
-            tasks[index].taskIsDone = false;
+            index = todo.tasks.findIndex((el) => el.taskDiscription === taskText);
+            todo.tasks[index].taskIsDone = false;
             $task.classList.toggle('done');
             event.target.classList.toggle('doneButton');
             event.target.innerHTML = '&#10008;';
-            saveToLS(tasks);
-
+            saveToLS('notes', todo);
         } else if (event.target.className === 'delete'){
             let result = confirm('Вы точно хотите удалить заметку?');
             if (result){
                 addSound.play();
-                index = tasks.findIndex((el) => el.taskDiscription === taskText);
-                tasks.splice(index, 1);
+                index = todo.tasks.findIndex((el) => el.taskDiscription === taskText);
+                todo.tasks.splice(index, 1);
                 $task.remove();
-                saveToLS(tasks);
+                saveToLS('notes', todo);
             }
-
         } else if (event.target.className === 'editButton') {
             let newText = prompt('Введите новый текст заметки', 'новый текст');
             if (newText !== '') {
-                let indexEdition = tasks.findIndex((el) => el.taskDiscription === newText);
+                let indexEdition = todo.tasks.findIndex((el) => el.taskDiscription === newText);
                 if (indexEdition === -1) {
                     let confirmEdition = confirm('Сохранить изменения?');
                     if (confirmEdition){
                         addSound.play();
-                        // toDo.editTask(taskText, newText);
-                        index = tasks.findIndex((el) => el.taskDiscription === taskText);
-                        tasks[index].taskDiscription = newText;
+                        index = todo.tasks.findIndex((el) => el.taskDiscription === taskText);
+                        todo.tasks[index].taskDiscription = newText;
                         taskBlock.innerText = newText;
-                        saveToLS(tasks);
+                        saveToLS('notes', todo);
                     }
                 } else {
                     alert('Такая заметка уже есть!');
                 }   
             } else {
                 alert('Нельзя вводить пустую заметку!');
-            } 
-            
+            }       
         }
-    })
+    });
 
     // HIDE/SHOW BUTTON
     // ---------------------
     // ---------------------
-
     $hideButton = document.querySelector('#hide');
     $hideButton.addEventListener('click', () => {
         addSound.play();
         if ($ul.className === 'notesList'){
-            $hideButton.innerText = 'Show all';
+            $hideButton.innerText = 'Show done';
+            todo.isShowDone = false;
+            saveToLS('notes', todo);
         } else if ($ul.className == 'notesList hidden') {
-            $hideButton.innerText = 'Hide all';
+            $hideButton.innerText = 'Hide done';
+            todo.isShowDone = true;
+            saveToLS('notes', todo);
         }
         $ul.classList.toggle('hidden');
-    })
+    });
